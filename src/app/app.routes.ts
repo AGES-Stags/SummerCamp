362a4a1e9e11e5a69a22fd0bc37abb212c5e2296
Cleyson Oliveira <cleyson.oliveira@pucrs.br>
2025-02-04 15:24:21 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { StudentComponent } from './student/student.component';
+import { HomeComponent } from './home/home.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'student', component: StudentComponent },
+  { path: '', component: HomeComponent },
+];
