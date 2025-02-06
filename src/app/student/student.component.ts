53b46ff3834c186b7364d21357d220da67499e13
Cleyson Oliveira <cleyson.oliveira@pucrs.br>
2025-02-06 17:04:43 -0300

    added integration

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,16 +1,25 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-student',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './student.component.html',
   styleUrl: './student.component.css',
 })
 export class StudentComponent {
   @ViewChild('studentIdInput') inputId!: ElementRef;
 
+  constructor(private http: HttpClient) {}
+
   requestStudent() {
-    console.log(this.inputId.nativeElement.value);
+    var id = this.inputId.nativeElement.value;
+    console.log(id);
+
+    var result = this.http.get(`http://localhost:8080/${id}`);
+    result.subscribe((value) => {
+      console.log(value);
+    });
   }
 }
